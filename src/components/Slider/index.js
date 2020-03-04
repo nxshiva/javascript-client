83718f400e561e48e2d0d35e118b43a3adedd1be
Shiva Sharma <shiva.sharma@lapr242.successivetech.com>
2020-03-04 19:44:21 +0530

    feat(src): introduction to state

---
+++ src/components/Slider/index.js
@@ -0,0 +1,5 @@
+import Slider from './Slider';
+import Img from './style';
+
+export default Slider;
+export { Img };
---
+++ src/configs/constant.js
@@ -0,0 +1,5 @@
+export const PUBLIC_IMAGE_FOLDER = '/images/';
+export const DEFAULT_BANNER_IMAGE = 'banners/default.png';
+
+export const banners = ['cloud.jpg', 'dns-server.png', 'full-stack-web-development.jpg', 'js.jpg', 'load-balancer.png'];
+export const defaultText = ['cloud', 'dns-server', 'full-stack-web-development', 'js', 'load-balancer'];
